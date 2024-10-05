e826d8426725c055f8438e8ae40e7c53b332d342
shubh2202 <63190729+shubh2202@users.noreply.github.com>
2024-10-06 02:38:06 +0530

    Create Records Working Fine.

---
+++ Backend-JavaSpring/src/main/java/com/development/Solar360/CreateRecords.java
@@ -0,0 +1,127 @@
+package com.development.Solar360;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import com.zoho.api.authenticator.OAuthToken;
+import com.zoho.api.authenticator.Token;
+import com.zoho.crm.api.HeaderMap;
+import com.zoho.crm.api.Initializer;
+import com.zoho.crm.api.dc.DataCenter.Environment;
+import com.zoho.crm.api.dc.INDataCenter;
+import com.zoho.crm.api.dc.USDataCenter;
+import com.zoho.crm.api.record.APIException;
+import com.zoho.crm.api.record.ActionHandler;
+import com.zoho.crm.api.record.ActionResponse;
+import com.zoho.crm.api.record.ActionWrapper;
+import com.zoho.crm.api.record.BodyWrapper;
+import com.zoho.crm.api.record.Field;
+import com.zoho.crm.api.record.RecordOperations;
+import com.zoho.crm.api.record.SuccessResponse;
+import com.zoho.crm.api.tags.Tag;
+import com.zoho.crm.api.util.APIResponse;
+import com.zoho.crm.api.util.Choice;
+import com.zoho.crm.api.util.Model;
+
+public class CreateRecords
+{
+	
+	public static void createRecords(String moduleAPIName) throws Exception
+	{
+		RecordOperations recordOperations = new RecordOperations(moduleAPIName);
+		BodyWrapper bodyWrapper = new BodyWrapper();
+		List<com.zoho.crm.api.record.Record> records = new ArrayList<com.zoho.crm.api.record.Record>();
+		com.zoho.crm.api.record.Record record1 = new com.zoho.crm.api.record.Record();
+		record1.addFieldValue(Field.Leads.LAST_NAME, "CHANDAK JAVA SDK");
+		record1.addFieldValue(Field.Leads.FIRST_NAME, "Shubham Java SDK");
+		record1.addFieldValue(Field.Leads.COMPANY, "KKRNP");
+		record1.addFieldValue(Field.Leads.ANNUAL_REVENUE, 1221.2);
+		record1.addFieldValue(Field.Leads.LEAD_STATUS, new Choice<String>("Cold"));
+		List<Tag> tagList = new ArrayList<Tag>();
+		Tag tag = new Tag();
+		tag.setName("Testtask");
+		tagList.add(tag);
+		record1.setTag(tagList);
+		records.add(record1);
+		bodyWrapper.setData(records);
+		HeaderMap headerInstance = new HeaderMap();
+		APIResponse<ActionHandler> response = recordOperations.createRecords(bodyWrapper, headerInstance);
+		if (response != null)
+		{
+			System.out.println("Status Code: " + response.getStatusCode());
+			if (response.isExpected())
+			{
+				ActionHandler actionHandler = response.getObject();
+				if (actionHandler instanceof ActionWrapper)
+				{
+					ActionWrapper actionWrapper = (ActionWrapper) actionHandler;
+					List<ActionResponse> actionResponses = actionWrapper.getData();
+					for (ActionResponse actionResponse : actionResponses)
+					{
+						if (actionResponse instanceof SuccessResponse)
+						{
+							SuccessResponse successResponse = (SuccessResponse) actionResponse;
+							System.out.println("Status: " + successResponse.getStatus().getValue());
+							System.out.println("Code: " + successResponse.getCode().getValue());
+							System.out.println("Details: ");
+							for (Map.Entry<String, Object> entry : successResponse.getDetails().entrySet())
+							{
+								System.out.println(entry.getKey() + ": " + entry.getValue());
+							}
+							System.out.println("Message: " + successResponse.getMessage().getValue());
+						}
+						else if (actionResponse instanceof APIException)
+						{
+							APIException exception = (APIException) actionResponse;
+							System.out.println("Status: " + exception.getStatus().getValue());
+							System.out.println("Code: " + exception.getCode().getValue());
+							System.out.println("Details: ");
+							for (Map.Entry<String, Object> entry : exception.getDetails().entrySet())
+							{
+								System.out.println(entry.getKey() + ": " + entry.getValue());
+							}
+							System.out.println("Message: " + exception.getMessage().getValue());
+						}
+					}
+				}
+				else if (actionHandler instanceof APIException)
+				{
+					APIException exception = (APIException) actionHandler;
+					System.out.println("Status: " + exception.getStatus().getValue());
+					System.out.println("Code: " + exception.getCode().getValue());
+					System.out.println("Details: ");
+					for (Map.Entry<String, Object> entry : exception.getDetails().entrySet())
+					{
+						System.out.println(entry.getKey() + ": " + entry.getValue());
+					}
+					System.out.println("Message: " + exception.getMessage().getValue());
+				}
+			}
+			else
+			{
+				Model responseObject = response.getModel();
+				Class<? extends Model> clas = responseObject.getClass();
+				java.lang.reflect.Field[] fields = clas.getDeclaredFields();
+				for (java.lang.reflect.Field field : fields)
+				{
+					System.out.println(field.getName() + ":" + field.get(responseObject));
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		try
+		{
+			Environment environment = INDataCenter.PRODUCTION;
+			Token token = new OAuthToken.Builder().accessToken("1000.cd146b854494d11b1ae0c86dd515fa7c.55ea55cf238d886f4e090c8596b0abf7").build();
+			new Initializer.Builder().environment(environment).token(token).initialize();
+			String moduleAPIName = "Leads";
+			createRecords(moduleAPIName);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
